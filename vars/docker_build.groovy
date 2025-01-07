23d5a438bfc4da313003002749bf25cb4748b157
sudhesh1993 <149937975+sudhesh1993@users.noreply.github.com>
2025-01-07 12:33:19 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,5 @@
 def call(string projectname, string imagetag, string dockerhubuser){
   echo 'Building Project'
   sh "docker build -t ${dockerhubuser}/${projectname}:${imagetag} ."
+  echo "cloning done!!"
 }
